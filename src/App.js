e6ca02b2d082e5a56880aa2c0c7307bef21021b4
codeSniper <codede95@outlook.com>
2021-07-27 16:32:35 +0200

    3rd day commit

--- src/App.js
+++ src/App.js
@@ -12,13 +12,15 @@ function App() {
     const [cart, setCart] = useState([])
     const [search, setSearch] = useState('')
     const [page, setPage] = useState('products')
+    let [itemCount, setItemCount] = useState(0)
+    let [total, setTotal] = useState(0)
 
-    var listof10 =[
-        'PT’s Coffee: Best dark roast','Angels’ Cup: Best Arabica','Angels’ Cup: Best Arabica'
-        ,'Lavazza: Best Italian','Don Francisco’s: Best-flavoure','Caribou Coffee: Best light roast'
-        ,'Folgers: Best USA coffee brand','Cafe Don Pablo: Best low acid','Bizzy Organic: Best cold brew'
-        ,'Nescafe Azera Intenso: Best instant coffee'
-       ]
+    var listof10 = [
+        'PT’s Coffee: Best dark roast', 'Angels’ Cup: Best Arabica', 'Angels’ Cup: Best Arabica'
+        , 'Lavazza: Best Italian', 'Don Francisco’s: Best-flavoure', 'Caribou Coffee: Best light roast'
+        , 'Folgers: Best USA coffee brand', 'Cafe Don Pablo: Best low acid', 'Bizzy Organic: Best cold brew'
+        , 'Nescafe Azera Intenso: Best instant coffee'
+    ]
 
     /*Methods */
     const handleSelect = (e) => {
@@ -45,12 +47,40 @@ function App() {
     const AddToCart = (product) => {
         setCart([...cart, { ...product }])
         setCmbValue(1)
+        setItemCount(itemCount + 1)
+        api.get(`/api/products/${product.productId}`).then((next) => {
+            console.log(next.data.productPrice)
+            setTotal(total + next.data.productPrice)
+        })
+    }
+    const RemoveFromCart = (productToRemove) => {
+        setCart(cart.filter((item) => item !== productToRemove))
+        setItemCount(itemCount - 1)
+        api.get(`/api/products/${productToRemove.productId}`).then((next) => {
+            console.log(next.data.productPrice)
+            setTotal(total - next.data.productPrice)
+        })
+    }
+
+    const sendOrder = () => {
+        api.post('/api/orders',{totalPrice:total,orderStatus:false}).then((res)=>{
+            console.log(res + 'orders added')
+        })
+        cart.map((orderItem) => {
+            api.get('/api/orders').then((i) => {
+                    api.post('/api/orderitems', {orderId:i.data.orderId +1,productId:orderItem.productId,quantity:cmbValue}).then((data) => {
+                        console.log(data.data) 
+                }).catch((err) =>console.log(err))
+            })
+
+        })
+
     }
 
     //RENDER UI COMPONENTS METHODS
     const renderProducts = () => (
         <div style={{ padding: '10px', margin: '10px' }}>
-            <h1 style={{ textAlign: 'center', fontSize: "50px",fontFamily:'fantasy' }}>Products</h1>
+            <h1 style={{ textAlign: 'center', fontSize: "50px", fontFamily: 'fantasy' }}>Products</h1>
             <div className="products">
                 {
                     products.filter((item) => {
@@ -78,12 +108,6 @@ function App() {
         </div>
     )
 
-    const RemoveFromCart = (productToRemove) => {
-        setCart(
-            cart.filter((item) => item !== productToRemove)
-        )
-    }
-
     const renderCart = () => {
 
         return (
@@ -95,7 +119,7 @@ function App() {
                                 cart.map((product) => {
                                     return (
                                         <>
-                                            <div class="card" style={{ width: '950px', padding: '5px', marginLeft: '40px', marginTop: '5px',marginBottom:'15px' }} key={product.productId}>
+                                            <div class="card" style={{ width: '950px', padding: '5px', marginLeft: '40px', marginTop: '5px', marginBottom: '15px' }} key={product.productId}>
                                                 <Row>
                                                     <Col sx lg="2">
                                                         <img style={{ width: '160px', height: '160px', alignContent: 'space-around' }} src={"https://localhost:7001/public/" + product.productImage} alt={product.productName} />
@@ -110,32 +134,32 @@ function App() {
                                                                     <p style={{ textAlign: 'right', fontSize: '28px' }} class="card.text">R{product.productPrice}</p>
                                                                     <Row>
                                                                         <Col>
-                                                                        <p  style={{marginTop: '5px',marginLeft:'270px', fontSize: '22px'}}>[{cmbValue}]</p>
+                                                                            <p style={{ marginTop: '5px', marginLeft: '270px', fontSize: '22px' }}>[{cmbValue}]</p>
                                                                         </Col>
                                                                         <Col>
-                                                                        <DropdownButton
-                                                                        variants="secondary"
-                                                                        title=""
-                                                                        id="dropdown-menu-align-right"
-                                                                        style={{textAlign: 'right', marginRight:'20px'}}
-                                                                        onSelect={handleSelect}>    
-                                                                        <Dropdown.Item eventKey="1">1</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="2">2</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="3">3</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="4">4</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="5">5</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="1">6</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="2">7</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="3">8</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="4">9</Dropdown.Item>
-                                                                        <Dropdown.Item eventKey="5">10</Dropdown.Item>
-                                                                    </DropdownButton>
+                                                                            <DropdownButton
+                                                                                variants="secondary"
+                                                                                title=""
+                                                                                id="dropdown-menu-align-right"
+                                                                                style={{ textAlign: 'right', marginRight: '20px' }}
+                                                                                onSelect={handleSelect}>
+                                                                                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="4">4</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="5">5</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="1">6</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="2">7</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="3">8</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="4">9</Dropdown.Item>
+                                                                                <Dropdown.Item eventKey="5">10</Dropdown.Item>
+                                                                            </DropdownButton>
                                                                         </Col>
                                                                     </Row>
                                                                 </Col>
                                                             </Row>
-                                                            <p style={{ textAlign: 'left', fontSize: '12px',padding:'5px' }} class="card.text">R{product.discription}</p>
-                                                            <p style={{textAlign:'right',marginRight: '10px'}} variant="secondary" size="md" onClick={() => RemoveFromCart(product)}>Remove</p>
+                                                            <p style={{ textAlign: 'left', fontSize: '12px', padding: '5px' }} class="card.text">R{product.discription}</p>
+                                                            <p style={{ textAlign: 'right', marginRight: '10px' }} variant="secondary" size="md" onClick={() => RemoveFromCart(product)}>Remove</p>
                                                         </div>
                                                     </Col>
                                                 </Row>
@@ -148,9 +172,16 @@ function App() {
                             <div class="card" style={{ width: '360px', padding: '20px', marginLeft: '20px' }}>
 
                                 <div class="card.body">
-                                    <h3 style={{ fontFamily: 'fantasy', fontSize: '18.5px' }} class="card-title"> Cart Summary</h3>
-                                    <p style={{ fontFamily: 'fantasy', fontSize: '18.5px' }} class="card.text">Total(item)</p>
-                                    <Button style={{ alignSelf: 'end' }} variant="btn btn-primary" size="md" >Proceed To CheckOut</Button>
+                                    <h3 style={{ fontFamily: 'fantasy', fontSize: '30px', marginRight: '0px' }} class="card-title"> Cart Summary</h3>
+                                    <Row sx lg="2">
+                                        <Col>
+                                            <p style={{ fontFamily: 'fantasy', fontSize: '18.5px', marginLeft: '50px', marginTop: '50px', textAlign: 'center' }} class="card.text">Total(item {itemCount})</p>
+                                        </Col>
+                                        <Col>
+                                            {total !== 0 ? <p style={{ fontFamily: 'fantasy', fontSize: '18.5px', marginRight: '0px', marginTop: '50px' }} class="card.text"> R{total}</p> : <p style={{ fontFamily: 'fantasy', fontSize: '18.5px', marginRight: '0px', marginTop: '50px' }} class="card.text"> R0,00</p>}
+                                        </Col>
+                                    </Row>
+                                    <Button onClick={()=> sendOrder()} style={{ alignSelf: 'end' }} variant="btn btn-primary" size="md" >Proceed To CheckOut</Button>
                                 </div>
                             </div>
                             <div class="card" style={{ width: '360px', padding: '20px', marginLeft: '20px', marginTop: '10px' }}>
@@ -169,26 +200,26 @@ function App() {
     }
 
     const renderSearchArea = () => (
-        
-        <div style={{ padding: '10px', margin: '10px',fontFamily:'fantasy'}}>
+
+        <div style={{ padding: '10px', margin: '10px', fontFamily: 'fantasy' }}>
             <h1 style={{ margin: '20px' }}>Search for coffee below</h1>
             <input tyle={{ margin: '20px' }} type="search" class="form-control" placeholder="Search" aria-label="Search"
                 aria-describedby="search-addon" placeholder="Search here..." onChange={(e) => {
                     setSearch(e.target.value);
                 }} />
 
-                <h5 style={{marginTop:'15px', color:'blue'}}>The 10 best coffee beans</h5>
-                {listof10.map((l)=>{
-                    return(
-                        <p style={{marginTop:'8px', fontSize:'16px'}}>{l}</p>
-                    )
-                })}
+            <h5 style={{ marginTop: '15px', color: 'blue' }}>The 10 best coffee beans</h5>
+            {listof10.map((l) => {
+                return (
+                    <p style={{ marginTop: '8px', fontSize: '16px' }}>{l}</p>
+                )
+            })}
         </div>
     )
 
     const NoItem = () => (
         <div style={{ fontFamily: 'fantasy' }} >
-            <h2 style={{ textAlign: 'justify', marginBottom: '30px',marginLeft:'100px' }}>Shopping Cart</h2>
+            <h2 style={{ textAlign: 'justify', marginBottom: '30px', marginLeft: '100px' }}>Shopping Cart</h2>
             <div style={{ width: '1200px', height: '300px', marginLeft: '100px' }} class="card">
                 <Image style={{ width: '140px', height: '140px', marginLeft: '500px', marginTop: '70px' }} src={gif} alt="no image" />
                 <p style={{ fontSize: '18.5px' }}>Your Shopping Cart is empty</p>
@@ -197,11 +228,16 @@ function App() {
         </div>
     )
 
+    const renderCheckout = () => (
+        <div lassName="">
+        </div>
+    )
+
     //Interface
     return (
         <div className="App" style={{ backgroundColor: '#F8F8F8' }}>
             <nav style={{ padding: '10px' }} class="navbar  navbar-expand-lg navbar-dark bg-dark justify-content-between" >
-                <a onClick={() => setPage('products')} style={{ padding: '10px', fontSize: '30px',fontFamily:'fantasy' }} class="navbar-brand"> DevCoffee Bean Store</a>
+                <a onClick={() => setPage('products')} style={{ padding: '10px', fontSize: '30px', fontFamily: 'fantasy' }} class="navbar-brand"> DevCoffeeBean(.co.za)</a>
                 <h5 onClick={() => setPage('cart')} class="my-2 my-sm-0 btn btn-primary" style={{ color: 'white', padding: '2px', marginRight: '10px' }}><i style={{ padding: '10px', borderRadius: 50 }} class="bi bi-cart"></i>({cart.length})</h5>
             </nav>
 
@@ -214,6 +250,7 @@ function App() {
                 </Col>
             </Row>
             {page === 'cart' && renderCart()}
+            {page === 'checkout' && renderCheckout()}
 
         </div >
     );
